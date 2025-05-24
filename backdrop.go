bc47e275c98781aa5bc38ff095becbd5d76fcc40
René Post <git@xpt.nl>
2025-05-24 09:24:59 +0200

    Add backdrop package with color utilities for Gio UI

---
+++ backdrop.go
@@ -0,0 +1,23 @@
+package gio
+
+import (
+	"image/color"
+
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+)
+
+func Fill(ops *op.Ops, fill color.Color) {
+	paint.ColorOp{Color: color.NRGBAModel.Convert(fill).(color.NRGBA)}.Add(ops)
+	paint.PaintOp{}.Add(ops)
+}
+
+func Widget(fill color.Color) layout.Widget {
+	return func(gtx layout.Context) layout.Dimensions {
+		defer clip.Outline{Path: clip.Rect{Max: gtx.Constraints.Max}.Path()}.Op().Push(gtx.Ops).Pop()
+		Fill(gtx.Ops, fill)
+		return layout.Dimensions{Size: gtx.Constraints.Max}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/vibrantgio/backdrop
+
+go 1.23.0
+
+require gioui.org v0.1.0
+
+require golang.org/x/sys v0.5.0 // indirect
